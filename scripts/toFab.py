9dfe0f702f819e69df3f433c7cdebbe3349bdc91
matthew venn <matt@mattvenn.net>
2012-03-08 12:11:21 +0000

    better error handling

--- scripts/toFab.py
+++ scripts/toFab.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import sys
 import shutil
+import os
 fileTypes = [ "dri", "drd", "drl", "GBL", "GBO", "GBS", "gpi", "GTL", "GTO", "GTP", "GTS", "TXT" ]
 
 if len(sys.argv)<2:
@@ -8,12 +9,18 @@ if len(sys.argv)<2:
 	exit( 1)
 
 fileStart = sys.argv[1]
-dest = "./toFab"
+dest = "./toFab/"
 print "using", fileStart
 for fileType in fileTypes: 
 	src = fileStart + "." + fileType
 	try:
-		shutil.move( src, dest )
+		os.remove(  dest + src  )
+	except Exception, e :
+		#do nothing
+		pass
+
+	try:
+		shutil.copy( src, dest )
 		print "moved %s -> %s" % ( src, dest )
 	except Exception, e :
 		print "file error: %s" % e
