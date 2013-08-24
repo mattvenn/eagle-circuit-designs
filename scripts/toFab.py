1f86160b8157f712b6f81f06ee07f7ea458d58ae
Matt Venn <matt@mattvenn.net>
2013-08-24 11:42:06 +0100

    more parts added

--- scripts/toFab.py
+++ scripts/toFab.py
@@ -5,23 +5,23 @@ import os
 fileTypes = [ "GM1", "dri", "drd", "GBL", "GBO", "GBS", "gpi", "GTL", "GTO", "GTP", "GTS", "TXT" ]
 
 if len(sys.argv)<2:
-	print "need name of file"
-	exit( 1)
+    print "need name of file"
+    exit( 1)
 
 fileStart = sys.argv[1]
 dest = "./toFab/"
 print "using", fileStart
 for fileType in fileTypes: 
-	src = fileStart + "." + fileType
-	try:
-		os.remove(  dest + src  )
-    print "removed ", dest + src
-	except Exception, e :
-		#do nothing
-		pass
+    src = fileStart + "." + fileType
+    try:
+        os.remove(  dest + src  )
+        print "removed: ", dest + src
+    except Exception, e :
+        #do nothing
+        pass
 
-	try:
-		shutil.copy( src, dest )
-		print "copied %s -> %s" % ( src, dest )
-	except Exception, e :
-		print "file error: %s" % e
+    try:
+        shutil.copy( src, dest )
+        print "copied %s -> %s" % ( src, dest )
+    except Exception, e :
+        print "file error: %s" % e
