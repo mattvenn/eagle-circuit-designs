ded5cab43cc0ece16d3b91efb598be3c20a50dc7
matthew venn <matt@mattvenn.net>
2014-04-02 13:07:45 +0100

    remove commas and added a quantity option

--- scripts/processBOM.py
+++ scripts/processBOM.py
@@ -5,6 +5,10 @@ if len(sys.argv)<2:
     print >>sys.stderr, "need bom file"
     exit( 1)
 bom_file = sys.argv[1]
+if len(sys.argv) == 3:
+    quantity = int(sys.argv[2])
+else:
+    quantity = 1
 
 rows = 0
 bad_rows = 0
@@ -22,7 +26,7 @@ with open(bom_file,'r') as csvfile:
                 print >> sys.stderr, "bad row length: ", row
                 bad_rows+=1
             else:
-                print ', '.join([row[3],row[5],row[4]])
+                print ', '.join([row[3],str(int(row[5])*quantity),row[4].replace(',','')])
         else:
             bad_rows += 1
 print >>sys.stderr ,"processed %d rows with %d missing rows" % (rows,bad_rows)
